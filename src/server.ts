7f66042b9a0adcc3a49cea003b317c2bca604b69
Luciano Dias <idh45043@gmail.com>
2021-07-18 19:10:40 +0100

    [RentalAPI] Add multer and swagger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/speficications.routes';
+import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log('Server running! 🚀'));
